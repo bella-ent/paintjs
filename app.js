3660903888118c27f7db6bad89ccac0a662d7030
Bella Yang <bellalucajosef@gmail.com>
2021-07-26 01:39:26 +0200

    Added random color-pick and save button

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const newColors = document.getElementsByClassName("newAddColors");
+const colorContainer = document.getElementById("jsColors");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
-const INITIAL_COLOR = "#2c2c2c";
 const saveBtn = document.getElementById("jsSave");
+const clearBtn = document.getElementById("jsClear");
+const color_front = document.getElementById("color_front");
+const colorPicker = document.getElementById("colorPicker");
+const colorSaveBtn = document.getElementById("saveRandom");
+const INITIAL_COLOR = "#2c2c2c";
+let colorSavedRandom = "#000000";
+let newColorArray = [];
+
+//? Random background colors
+const BG_colors = [
+	"#ABDEE6",
+	"#CBAACB",
+	"#FFFFB5",
+	"#55CBCD",
+	"#FF968A",
+	"#FFDBCC",
+	"#ECEAE4",
+	"#f6f9fc",
+];
+const chosen_bgColor = BG_colors[Math.floor(Math.random() * BG_colors.length)];
+
+document.body.style.backgroundColor = chosen_bgColor;
 
 canvas.width = 700;
 canvas.height = 600;
 
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
+ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -81,6 +104,35 @@ function handleSaveClick() {
 	link.click();
 }
 
+function clearCanvas() {
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function clickInput() {
+	colorPicker.click();
+}
+
+function changeColor(event) {
+	color_front.style.backgroundColor = event.target.value;
+	colorSavedRandom = event.target.value;
+	ctx.strokeStyle = colorSavedRandom;
+	ctx.fillStyle = colorSavedRandom;
+}
+
+function saveColor() {
+	const newColor = document.createElement("div");
+	newColor.classList.add("controls__color");
+	newColor.classList.add("jsColor");
+	newColor.classList.add("newAddColors");
+	newColor.style.backgroundColor = colorSavedRandom;
+	colorContainer.appendChild(newColor);
+	newColorArray.push(newColor);
+	newColorArray.forEach((color) =>
+		color.addEventListener("click", handleColorClick)
+	);
+}
+
 if (canvas) {
 	canvas.addEventListener("mousemove", onMouseMove);
 	canvas.addEventListener("mousedown", startPainting);
@@ -104,4 +156,20 @@ if (mode) {
 
 if (saveBtn) {
 	saveBtn.addEventListener("click", handleSaveClick);
+}
+
+if (clearBtn) {
+	clearBtn.addEventListener("click", clearCanvas);
+}
+
+if (color_front) {
+	color_front.addEventListener("click", clickInput);
+}
+
+if (colorPicker) {
+	colorPicker.addEventListener("change", changeColor);
+}
+
+if (colorSaveBtn) {
+	colorSaveBtn.addEventListener("click", saveColor);
 }
